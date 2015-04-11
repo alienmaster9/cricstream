193ede84251c6a1a80cd3c6829c0a0da491258e7
Venkatesh Gajula <quickstreamingmedia@gmail.com>
2015-04-11 10:30:52 +0530

    tezf
    sfsdfsd

---
+++ Player.js
@@ -0,0 +1,3 @@
+// JScript File
+document.write('<ifr'+'ame src="http://www.cricketstream.in/embed.php?v='+ fid +'&vw='+v_width+'&vh='+v_height+'" width='+ v_width +' height=' + v_height + ' scrolling=no frameborder=0 scrolling=no allowtransparency=true id=thatframe ></ifr'+'ame>') ;
+/**/
